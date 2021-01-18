99865fb85164837d183e2e8c6f0c8926e9d6e00e
AgaK1 <AgaK1@users.noreply.github.com>
2021-01-18 12:02:33 +0100

    Python-exercises

---
+++ 7-Kyuu/Credit_card_mask.py
@@ -0,0 +1,14 @@
+# Your task is to write a function maskify, which changes all but the last four characters into '#'.
+
+# Examples
+# maskify("4556364607935616") == "############5616"
+# maskify(     "64607935616") ==      "#######5616"
+# maskify(               "1") ==                "1"
+# maskify(                "") ==                 ""
+
+# # "What was the name of your first pet?"
+# maskify("Skippy")                                   == "##ippy"
+# maskify("Nananananananananananananananana Batman!") == "####################################man!"
+
+def maskify(cc):
+    return "#"*(len(cc)-4) + cc[-4:]
